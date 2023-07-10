d8a1dc133888af4e366cb9561506e320ad64656b
magwoo <magwood51@yandex.ru>
2023-07-10 21:26:57 +0300

    update one file server

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,43 @@
 use handlebars::Handlebars;
-use serde::Serialize;
 use std::{
-    io::{BufRead, BufReader, Write},
+    collections::BTreeMap,
+    io::{prelude::*, BufReader},
     net::{TcpListener, TcpStream},
 };
 
-#[derive(Serialize)]
-struct IndexPage {
-    data: String,
-}
-
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let mut handlebars = Handlebars::new();
+    handlebars
+        .register_templates_directory(".hbs", "templates/")
+        .unwrap();
 
     for stream in listener.incoming() {
-        println!("connected");
-        handle_connection(stream.unwrap());
+        let stream = stream.unwrap();
+
+        handle_connection(stream, &handlebars);
     }
 }
 
-fn handle_connection(mut stream: TcpStream) {
+fn handle_connection(mut stream: TcpStream, handlebars: &Handlebars) {
     let buf_reader = BufReader::new(&mut stream);
-    let mut handlebars = Handlebars::new();
-    let _request_line = buf_reader.lines().next().unwrap().unwrap();
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
+    let mut data: BTreeMap<&str, String> = BTreeMap::new();
 
-    handlebars
-        .register_template_file("index", "templates/index.hbs")
-        .unwrap();
-
-    let data = IndexPage {
-        data: "hello".to_string(),
+    let (status_line, content) = if request_line == "GET / HTTP/1.1" {
+        data.insert("data", "world".to_string());
+        (
+            "HTTP/1.1 200 OK",
+            handlebars.render("index", &data).unwrap(),
+        )
+    } else {
+        (
+            "HTTP/1.1 404 NOT FOUND",
+            handlebars.render("404", &data).unwrap(),
+        )
     };
 
-    let content = handlebars.render("index", &data).unwrap();
-
-    let response = format!("HTTP/1.1 200 OK\n\n{content}");
+    let response = format!("{status_line}\n\n{content}");
 
     stream.write_all(response.as_bytes()).unwrap();
 }
